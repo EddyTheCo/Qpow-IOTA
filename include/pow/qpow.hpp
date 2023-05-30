cf7011e01be6306f6c2be334c64a22ccb4e4ae15
EddyGlezz <eddyglezz95@gmail.com>
2023-05-30 20:31:25 +0200

    added explicit export_import for windows

--- include/pow/qpow.hpp
+++ include/pow/qpow.hpp
@@ -10,12 +10,20 @@
 #include<thread>
 #include<mutex>
 #endif
+
+#include <QtCore/QtGlobal>
+
+#if defined(MYSHAREDLIB_LIBRARY)
+# define MYSHAREDLIB_EXPORT Q_DECL_EXPORT
+#else
+#define MYSHAREDLIB_EXPORT Q_DECL_IMPORT
+#endif
 namespace qiota{
 
 	namespace qpow {
 
 
-		class nonceFinder : public QObject
+		class MYSHAREDLIB_EXPORT nonceFinder : public QObject
 		{
 			Q_OBJECT
 			public:
