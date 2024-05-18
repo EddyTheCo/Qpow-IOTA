05a658ac3d6b3db482b6f5a96cd0c57f8189d97b
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-05-18 11:22:26 +0200

    Develop (#77)
    * install dlls on bin dir (#74)
    
    * use latest commons, and added format action (#75)
    
    * C/C++/QML formatted code (#76)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- include/pow/qpow.hpp
+++ include/pow/qpow.hpp
@@ -4,45 +4,46 @@
  *
  **/
 
-#include<QByteArray>
-#include<QObject>
+#include <QByteArray>
+#include <QObject>
 #ifdef USE_THREADS
-#include<thread>
-#include<mutex>
+#include <mutex>
+#include <thread>
 #endif
 
 #include <QtCore/QtGlobal>
 
 #if defined(WINDOWS_QPOW)
-# define QPOW_EXPORT Q_DECL_EXPORT
+#define QPOW_EXPORT Q_DECL_EXPORT
 #else
 #define QPOW_EXPORT Q_DECL_IMPORT
 #endif
-namespace qiota{
+namespace qiota
+{
 
-	namespace qpow {
+namespace qpow
+{
 
+class QPOW_EXPORT nonceFinder : public QObject
+{
+    Q_OBJECT
+  public:
+    nonceFinder();
 
-		class QPOW_EXPORT nonceFinder : public QObject
-		{
-			Q_OBJECT
-			public:
-				nonceFinder();
+    void calculate(const QByteArray &Message);
+    void set_Min_Pow_Score(quint32 Min_PoW_Score_m)
+    {
+        Min_PoW_Score_ = Min_PoW_Score_m;
+    };
 
-				void calculate(const QByteArray &Message);
-				void set_Min_Pow_Score(quint32 Min_PoW_Score_m){Min_PoW_Score_=Min_PoW_Score_m;};
+  signals:
+    void nonce_found(quint64 s);
+    void nonce_not_found(void);
 
+  private:
+    quint64 thenonce, NThreads;
+    quint32 Min_PoW_Score_;
+};
 
-			signals:
-				void nonce_found(quint64 s);
-				void nonce_not_found(void);
-
-			private:
-				quint64 thenonce,NThreads;
-				quint32 Min_PoW_Score_;
-
-		};
-
-
-	}
-}
+} // namespace qpow
+} // namespace qiota
